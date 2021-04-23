56125def4cbfcea557e12a43cd1670afbd645adf
Shavonne Holton <shavonne.holton@gmail.com>
2021-04-23 07:47:01 -0700

    pulled text from each object

--- script.js
+++ script.js
@@ -1,25 +1,16 @@
 const displayCatFacts = () => {
 const baseURL = 'https://cat-fact.herokuapp.com/facts'
-console.log('Hello World!')
     fetch(baseURL)
         .then(res => res.json())
         .then(json => {
             console.log(json)
-        })
-    var text = [] 
-    for(let i=0; i<text.length; i++){
-        console.log(text)
-    }
+        var x = json.map(cat => {
+            console.log(cat.text)
+        }) 
+    })
 }
-
     
 let onClickButton = document.getElementById('testing-onclick')
 onClickButton.onclick = displayCatFacts
 
-// const displayCat = () => {
-//     const wrapper = document.getElementById('facts')
-//     let fact = res.results[0].text
-//     console.log(fact)
-// }
-
-// use "map" or forEach 
+// append
